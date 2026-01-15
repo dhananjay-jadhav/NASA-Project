edcc67452e66f9a2c177bfe6072e0e83bdd6ca8f
dhananjay-jadhav <jadhav9260@gmail.com>
2026-01-15 21:47:50 +0530

    Bundle client and server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 
 import planetsRoute from './routes/planets.routes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -9,6 +14,8 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }))
 app.use(express.json());
+
+app.use(express.static(join(__dirname, "..", "public")));
 
 app.use('/planets', planetsRoute);
 
