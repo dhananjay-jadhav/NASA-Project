b923668f1e4acc31a6e37c41538a382963c216da
dhananjay-jadhav <jadhav9260@gmail.com>
2026-01-16 09:48:48 +0530

    loggin

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 
 import planetsRoute from './routes/planets.routes.js';
+import morgan from 'morgan';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,14 +14,17 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
+
 app.use(express.json());
 
+app.use(morgan('combined'));
+
 app.use(express.static(join(__dirname, "..", "public")));
+
+app.use('/planets', planetsRoute);
 
 app.use('/',(req, res) => {
     res.sendFile(join(__dirname, "..", "public", 'index.html'))
 });
-
-app.use('/planets', planetsRoute);
 
 export default app;
